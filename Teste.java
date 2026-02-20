4add13bef8ec7ad4cf5760561609282800fb05b5
Victor Reis <reisdasilva.victor@outlook.com>
2026-02-19 22:48:01 -0300

    add test class

---
+++ Teste.java
@@ -0,0 +1,22 @@
+public class Teste {
+    public static void main(String [] args){
+
+        
+        Carro x = new Carro();
+
+        x.setCategoria("sedan");
+        x.setAno(2001);
+        x.setModelo("civic");
+        x.setMarca("Honda");
+
+        Carro y = new Carro();
+        y.setMarca("toyota");
+        y.setModelo("RAV4");
+        y.setCategoria("SUV");
+        y.setAno(2015);
+
+
+
+    }
+
+}
