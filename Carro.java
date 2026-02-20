d6107e82f0b802420d03a77db2bdff53050e8b4c
Victor Reis <reisdasilva.victor@outlook.com>
2026-02-19 22:48:40 -0300

    Add Carro class with basic attributes and methods

---
+++ Carro.java
@@ -0,0 +1,40 @@
+
+
+public class Carro{
+    private  String Marca;
+    private int Ano;
+    private String Modelo;
+    public String Categoria;
+
+    public  String getMarca(){
+        return Marca;
+
+    }
+    public void setMarca(String Marca){
+        this.Marca = Marca;
+    } 
+
+    public  String getModelo(){
+        return Marca;
+
+    }
+    public void  setModelo(String modelo){
+        this.Modelo = modelo;
+    } 
+
+    public  String getCategoria(String Categoria){
+        return Marca;
+
+    }
+    public void setCategoria( String categoria){
+        this.Categoria = categoria;
+    } 
+    public  int getAno(){
+        return Ano;
+
+    }
+    public void setAno(int Ano){
+        this.Ano = Ano;
+    } 
+}
+
