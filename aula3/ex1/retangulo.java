80049c0dffc9e1b5b4fd5ff8deddb38f40e494ec
Victor Reis <reisdasilva.victor@outlook.com>
2026-02-27 01:38:11 +0000

    ex1

---
+++ aula3/ex1/retangulo.java
@@ -0,0 +1,34 @@
+package ex1;
+
+
+public class retangulo{
+   private float largura= 1;
+   private float altura=1;
+
+    // getter e setters
+
+    public float getAltura(){ return altura;  }
+    public void setAltura(float altura){ this.altura= altura; }
+
+    public float getLargura(){return largura;}
+    public void setLargura(float largura){this.largura = largura;}
+
+    public void retangulo(){
+        retangulo r = new retangulo();
+        r.setAltura(altura);
+        r.setLargura(largura);
+        r.close();
+
+
+
+    }
+    public retangulo(float altura, float largura){
+        setAltura(altura);
+
+    }
+
+}
+
+
+
+
---
+++ aula3/ex1/testeRetangulo.java
@@ -0,0 +1,15 @@
+package aula3.ex1;
+import java.util.Scanner;
+
+public class testeRetangulo {
+    public static void main(String [] args){
+        
+        float a= r.getAltura;
+        float l= r.getLargura();
+
+    }
+        
+        
+        
+    }
+    
